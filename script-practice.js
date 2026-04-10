2592f4f4bfac087b37331a5975ccaf323985940d
Ali Ahmad <aliahmed192181@gmail.com>
2026-04-10 12:49:01 +0500

    Update script-practice.js

--- script-practice.js
+++ script-practice.js
@@ -532,32 +532,507 @@ const questionBank = {
     question: "Which state was attacked by India on 11th September 1948?",
     options: ["State of Hyderabad Daccan", "State of Jammu and Kashmir", "State of Manavadar", "State of Junagarh"],
     answer: 0
+  }, 
+  {
+    question: "In which year the Kashmiris started their freedom movement against Dogra rule before the partition of the sub-continent?",
+    options: ["1940", "1930", "1920", "1928"],
+    answer: 1
+  }, // [1]
+  {
+    question: "In which of the following cities Indian National Congress was found?",
+    options: ["Delhi", "Bombay", "Kanpur", "Allahabad"],
+    answer: 1
+  }, // [1]
+  {
+    question: "In which of the following cities All India Muslim League was found?",
+    options: ["Delhi", "Lahore", "Dhaka", "Allahabad"],
+    answer: 2
+  }, // [1, 2]
+  {
+    question: "Who was the Viceroy of India at the time of partition of Bengal?",
+    options: ["Lord Curzon", "Lord Wavell", "Lord Minto", "Lord Algan"],
+    answer: 0
+  }, // [2]
+  {
+    question: "Who was the first President of All India Muslim League?",
+    options: ["Sir Agha Khan", "Nawab Waqar ul Mulk", "Nawab Saleem ullah Khan", "None of them"],
+    answer: 0
+  }, // [2]
+  {
+    question: "In which year Ali Garh Trusteeship bill was passed?",
+    options: ["1886", "1887", "1888", "1889"],
+    answer: 3
+  }, // [2]
+  {
+    question: "In which year Syed Jamal-u-din Afghani died?",
+    options: ["1895", "1896", "1897", "1898"],
+    answer: 2
+  }, // [2, 3]
+  {
+    question: "What was the real name of Titu Mir?",
+    options: ["Nisar Ali", "Sayed Ahmad", "Muhammad Ali", "None of them"],
+    answer: 0
+  }, // [3]
+  {
+    question: "Which pass connects Pakistan with Afghanistan?",
+    options: ["Khunjerab Pass", "Khyber Pass", "Tochi Pass", "Gomal Pass"],
+    answer: 1
+  }, // [3]
+  {
+    question: "The highest peak of Salt Range is?",
+    options: ["Nanga Parbat", "Skaser", "Malka Parbat", "Everest"],
+    answer: 1
+  }, // [3]
+  {
+    question: "Which mountain range is located in Sindh Province?",
+    options: ["Himalaya", "Karakoram Range", "Kirthar Range", "Salt Range"],
+    answer: 2
+  }, // [3, 4]
+  {
+    question: "Pakistan conducted nuclear tests on May 28, 1998 at?",
+    options: ["Kohe-e-Sufaid", "Chaghi Hills", "Toba Kakar", "Raskoh"],
+    answer: 1
+  }, // [4]
+  {
+    question: "What is the total area of Punjab Province?",
+    options: ["74,521 Km", "40,914 Km", "347,190 Sq Km", "205,344 Sq Km"],
+    answer: 3
+  }, // [4]
+  {
+    question: "The capital city of Gilgit Baltistan is?",
+    options: ["Ghanchay", "Nagar", "Chilas", "Gilgit"],
+    answer: 3
+  }, // [4]
+  {
+    question: "Who was the founder of Daily English Newspaper Dawn?",
+    options: ["Liaquat Ali Khan", "Z.A Sulehri", "Quaid-e-Azam", "None of them"],
+    answer: 2
+  }, // [4, 5]
+  {
+    question: "Pakistan Steel Mills is located in?",
+    options: ["Lahore", "Gwadar", "Karachi", "None of them"],
+    answer: 2
+  }, // [5]
+  {
+    question: "Who was the second Prime Minister of Pakistan?",
+    options: ["Ch Muhammad Ali", "Kh. Nazim-ud-Din", "Liaquat Ali Khan", "Feroze Khan Noon"],
+    answer: 1
+  }, // [5]
+  {
+    question: "The 1973 Constitution of Pakistan came into force on?",
+    options: ["23rd March 1973", "14th August 1973", "8th June 1973", "27th October 1973"],
+    answer: 1
+  }, // [5]
+  {
+    question: "The 1962 Constitution of Pakistan was passed during the regime of?",
+    options: ["Sikandar Mirza", "Ayoub Khan", "Yahya Khan", "Ch. Muhammad Ali"],
+    answer: 1
+  }, // [5]
+  {
+    question: "ICT stands for?",
+    options: ["Islamabad capital total", "Islamabad city tribes", "Islamabad Capital Territory", "Islamabad city territory"],
+    answer: 2
+  }, // [6]
+  {
+    question: "First Charter of Human Rights is?",
+    options: ["International Charter", "U.N. Charter", "Khutba-Hajjat-ul-Vida", "French Constitution"],
+    answer: 2
+  }, // [6]
+  {
+    question: "The members of Punjab Assembly are?",
+    options: ["275", "371", "237", "100"],
+    answer: 1
+  }, // [6]
+  {
+    question: "Tenure of National and Provincial Assemblies is?",
+    options: ["4 years", "5 years", "6 years", "3 years"],
+    answer: 1
+  }, // [6, 7]
+  {
+    question: "Number of houses of Pakistan’s Majlis-e-Shura (Parliament) is?",
+    options: ["One", "Two", "Three", "Four"],
+    answer: 1
+  }, // [7]
+  {
+    question: "The Supreme commander of Pakistan Armed Forces is?",
+    options: ["Army Chief", "Prime Minister", "President", "Governor"],
+    answer: 2
+  }, // [7]
+  {
+    question: "SNGPL stands for?",
+    options: ["Sui Northern Gas Pipelines Lahore", "Sui Northern Gas Pipelines limited", "Sui Northern Gas Pakistan limited", "Sui Northern Gas Punjab limited"],
+    answer: 1
+  }, // [7]
+  {
+    question: "The third largest city of Pakistan is?",
+    options: ["Faisalabad", "Rawalpindi", "Sialkot", "Hyderabad"],
+    answer: 0
+  }, // [8]
+  {
+    question: "First Census was held in Pakistan in the year?",
+    options: ["1950", "1951", "1952", "1953"],
+    answer: 1
+  }, // [8]
+  {
+    question: "Sadqain is famous for?",
+    options: ["Painting", "Calligraphy", "Music", "A & B"],
+    answer: 3
+  }, // [8]
+  {
+    question: "The area between rivers Jhelum and Chenab is called?",
+    options: ["Gandhara", "Chaj", "Taxila", "Harrapa"],
+    answer: 1
+  }, // [8]
+  {
+    question: "The largest Railway workshop Mughalpura is situated in?",
+    options: ["Gujranwala", "Multan", "Karachi", "Lahore"],
+    answer: 3
+  }, // [9]
+  {
+    question: "The ruins of Harrapa are situated in district?",
+    options: ["Multan", "Okara", "Lahore", "Sahiwal"],
+    answer: 3
+  }, // [9]
+  {
+    question: "Abdul Rehman Chughtai was attached with the art of?",
+    options: ["Construction", "Music", "Painting", "None of them"],
+    answer: 2
+  }, // [9]
+  {
+    question: "Which is the sixth most populated country of the world?",
+    options: ["India", "Iran", "Pakistan", "None of them"],
+    answer: 2
+  }, // [9]
+  {
+    question: "Thal desert is located in?",
+    options: ["Sindh", "Gilgit", "KPK", "Punjab"],
+    answer: 3
+  }, // [10]
+  {
+    question: "The founder of All India Muslim League Nawab Salimullah Khan belonged to?",
+    options: ["Madras", "Karachi", "Dhaka", "Delhi"],
+    answer: 2
+  }, // [10]
+  {
+    question: "Who is the first Punjab poet?",
+    options: ["Baba Farid", "Shah Hussain", "Bullhay Shah", "None of them"],
+    answer: 0
+  }, // [10]
+  {
+    question: "Who was the hero of Pak India 1965 war?",
+    options: ["Major Raja Aziz Bhatti", "Major Tufail", "Lalik Jan", "Mulla Faqir"],
+    answer: 0
+  }, // [10, 11]
+  {
+    question: "The name of poet whose collection of poetry is called 'Shah jo Risalu'?",
+    options: ["Khushal Khan Khattak", "Waris Shah", "Makhdum Muhammad Hashim", "Shah Abdul Latif Bhatai"],
+    answer: 3
+  }, // [11]
+  {
+    question: "Which is the first book of Pushto language?",
+    options: ["Pata Khazana", "Tazkra-tul-Aulia", "Jat di Kartut", "Aasar-ul-Sanadeed"],
+    answer: 0
+  }, // [11]
+  {
+    question: "Urdu is a word of Turkish language, it means?",
+    options: ["Wth", "Arms", "Army", "Literature"],
+    answer: 2
+  }, // [11]
+  {
+    question: "Who wrote 'Mauj-e-Kausar'?",
+    options: ["Amir Khusro", "Shaikh Muhammad Ikram", "Mir Taqi Mir", "Khawaja Mir Dard"],
+    answer: 1
+  }, // [11, 12]
+  {
+    question: "Name the language in which the Holy Quran was translated for the first time in India?",
+    options: ["Punjabi", "Kashmiri", "Sindhi", "Balochi"],
+    answer: 2
+  }, // [12]
+  {
+    question: "Madr-e-Millat Fatima Jinnah died in the year?",
+    options: ["1940", "1950", "1967", "1970"],
+    answer: 2
+  }, // [12]
+  {
+    question: "Ahmad Shah Pitras Bukhari was a famous writer of ______ language.",
+    options: ["English", "Hindi", "Urdu", "Punjabi"],
+    answer: 2
+  }, // [12]
+  {
+    question: "Who inaugurated the State Bank of Pakistan?",
+    options: ["Quaid-e-Azam", "Malik Ghulam Muhammad", "Liaquat Ali Khan", "Allama Muhammad Iqbal"],
+    answer: 0
+  }, // [13]
+  {
+    question: "Sardar Abdur Rub Nishtar was the Governor of?",
+    options: ["Gilgit Baltistan", "KPK", "Punjab", "Sindh"],
+    answer: 2
+  }, // [13]
+  {
+    question: "National code of Pakistan is?",
+    options: ["PAK", "PK", "PAK 1", "None of them"],
+    answer: 1
+  }, // [13]
+  {
+    question: "Where is Warsak Dam of Pakistan situated?",
+    options: ["Khyber Pakhtunkhwa", "Punjab", "Sindh", "Balochistan"],
+    answer: 0
+  }, // [13, 14]
+  {
+    question: "Identify the largest cantonment of Pakistan?",
+    options: ["Kharian Cantt", "Quetta Cantt", "Okara Cantt", "Karachi Cantt"],
+    answer: 0
+  }, // [14]
+  {
+    question: "Which city of Pakistan held the OIC Conference in 1997?",
+    options: ["Lahore", "Islamabad", "Karachi", "Peshawar"],
+    answer: 1
+  }, // [14]
+  {
+    question: "In which year was OIC founded?",
+    options: ["1970", "1975", "1980", "1969"],
+    answer: 3
+  }, // [14]
+  {
+    question: "Length of common border of Pakistan and Afghanistan is?",
+    options: ["2252 Km", "2282 Km", "2350 Km", "2430 Km"],
+    answer: 3
+  }, // [14]
+  {
+    question: "The Kargil incident happened in?",
+    options: ["1998", "1997", "1999", "2000"],
+    answer: 2
+  }, // [15]
+  {
+    question: "Which country assisted Pakistan in the construction of Sandak Project?",
+    options: ["Iran", "Saudi Arabia", "Afghanistan", "China"],
+    answer: 3
+  }, // [15]
+  {
+    question: "When was Liaquat Nehru Pact between India and Pakistan signed?",
+    options: ["1950", "1955", "1964", "1951"],
+    answer: 0
+  }, // [15]
+  {
+    question: "Who was the First woman Governor of State Bank of Pakistan?",
+    options: ["Noor Jahan", "Dr Shamshad Akhter", "Farzana Raja", "Dr Ashraf Abbasi"],
+    answer: 1
+  }, // [15]
+  {
+    question: "Mirpur is a famous city of?",
+    options: ["KPK", "Punjab", "Azad Kashmir", "None of them"],
+    answer: 2
   }, // [16]
-    // {
-    //   question: "Who was the founder of the Ottoman Empire?",
-    //   options: ["Osman I", "Suleiman the Magnificent", "Mehmed II", "Selim I"],
-    //   answer: 0
-    // },
-    // {
-    //   question: "Which ancient civilization built the Pyramids?",
-    //   options: ["Mesopotamians", "Romans", "Egyptians", "Greeks"],
-    //   answer: 2
-    // },
-    // {
-    //   question: "Who was the first Mughal Emperor of India?",
-    //   options: ["Humayun", "Akbar", "Babur", "Aurangzeb"],
-    //   answer: 2
-    // },
-    // {
-    //   question: "Who painted the Mona Lisa?",
-    //   options: ["Michelangelo", "Raphael", "Leonardo da Vinci", "Donatello"],
-    //   answer: 2
-    // },
-    // {
-    //   question: "Which war was fought between the North and South regions of the United States?",
-    //   options: ["American Civil War", "World War I", "Revolutionary War", "Cold War"],
-    //   answer: 0
-    // }
+  {
+    question: "What is the total area of Balochistan Province?",
+    options: ["74,521 Sq Km", "40,914 Sq Km", "347,190 Sq Km", "205,344 Sq Km"],
+    answer: 2
+  }, // [16]
+  {
+    question: "What is the total area of Sindh Province?",
+    options: ["74,521 Sq Km", "140,914 Sq Km", "347,190 Sq Km", "205,344 Sq Km"],
+    answer: 1
+  }, // [16]
+  {
+    question: "What is the total area of Khyber Pakhtoon Khawah Province?",
+    options: ["74,521 Sq Km", "40,914 Sq Km", "347,190 Sq Km", "205,344 Sq Km"],
+    answer: 0
+  }, // [16]
+  {
+    question: "What is the total area of FATA?",
+    options: ["27,220 Sq Km", "40,914 Sq Km", "347,190 Sq Km", "205,344 Sq Km"],
+    answer: 0
+  }, // [16, 17]
+  {
+    question: "What is the total area of ICT?",
+    options: ["906 Sq Km", "1106 Sq Km", "1006 Sq Km", "1206 Sq Km"],
+    answer: 0
+  }, // [17]
+  {
+    question: "GHQ Stands for:",
+    options: ["General Head Quarters", "General High Quarters", "General High Quartermaster", "None of them"],
+    answer: 0
+  }, // [17]
+  {
+    question: "KRL Stands for:",
+    options: ["Khan Research Laboratories", "Kahuta Research Laboratories", "Karachi Research Laboratories", "None of them"],
+    answer: 1
+  }, // [17]
+  {
+    question: "Who was the first C-in-C of Pakistan Navy?",
+    options: ["Gefford James", "R.L Archy", "Frank Meseri", "None of them"],
+    answer: 0
+  }, // [17, 18]
+  {
+    question: "Frank Meseri was the first C-in-C of ________ .",
+    options: ["Pak Army", "Air Force", "Navy", "None of them"],
+    answer: 0
+  }, // [18]
+  {
+    question: "Paery Cane was the first C-in-C of __________.",
+    options: ["Pakistan Air Force", "Armed Forces", "Navy", "None of them"],
+    answer: 2
+  }, // [18]
+  {
+    question: "The first Chief of Staff of Pak Army was ________ ?",
+    options: ["General Takka Khan", "General Ziaul Haq", "General Sharif", "General Ayub Khan"],
+    answer: 0
+  }, // [18, 19]
+  {
+    question: "Zafar Ahmed Chaukhry was the first Chief of Air Staff of _______ .",
+    options: ["Pak Army", "Air Force", "Navy", "None of them"],
+    answer: 1
+  }, // [19]
+  {
+    question: "Defence day is observed on ___________ ?",
+    options: ["6th September", "11th September", "7th September", "None of them"],
+    answer: 0
+  }, // [19]
+  {
+    question: "The headquarters of Air Force is located in _________ ?",
+    options: ["Islamabad", "Rawalpindi", "Peshawar", "Karachi"],
+    answer: 0
+  }, // [19, 20]
+  {
+    question: "The highest official rank of Air Force is ___________ ?",
+    options: ["Air Marshal", "Supreme Marshal", "Air Chief Marshal", "Chief of Air Force"],
+    answer: 2
+  }, // [20]
+  {
+    question: "The highest non-operational Military Award is _________ ?",
+    options: ["Tamgha-e-Basalat", "Tamgha-e-Imtiaz", "Tamgha-e-Khidmat", "Sitara-e-Basalat"],
+    answer: 1
+  }, // [20]
+  {
+    question: "Pakistan tested its nuclear device on __________ ?",
+    options: ["28th May 1998", "26th May 1997", "28th June 1999", "27th July 1997"],
+    answer: 0
+  }, // [20, 21]
+  {
+    question: "Baktar Shikan missile system is __________ ?",
+    options: ["Air to surface", "Anti aircraft", "Anti tank guided", "Anti tank"],
+    answer: 2
+  }, // [21]
+  {
+    question: "The range of Hatf-1 is _________ ?",
+    options: ["90km", "100km", "65km", "105km"],
+    answer: 1
+  }, // [21]
+  {
+    question: "The range if Hatf-2 is ________ ?",
+    options: ["180km", "275km", "230km", "300km"],
+    answer: 0
+  }, // [21]
+  {
+    question: "The range if Hatf-3 is ________ ?",
+    options: ["400km", "290km", "600km", "750km"],
+    answer: 1
+  }, // [21]
+  {
+    question: "The range if Hatf-4 is _______ ?",
+    options: ["400km", "390km", "600km", "750km"],
+    answer: 3
+  }, // [21, 22]
+  {
+    question: "The latest version of Hatf-5 is ________ ?",
+    options: ["Ghauri-1", "Hataf", "Abdali", "Shaheen-1"],
+    answer: 0
+  }, // [22]
+  {
+    question: "The Range of Ghari-1 is ________ ?",
+    options: ["1200-1500km", "1200-1300km", "1500-2000km", "1500-1800km"],
+    answer: 0
+  }, // [22]
+  {
+    question: "The highest official rank of Pakistan Army is _________ ?",
+    options: ["Lieutenant", "Major General", "General", "None of them"],
+    answer: 2
+  }, // [22]
+  {
+    question: "Who was the first Muslim C-in-C of Pakistan Army?",
+    options: ["Admiral Muhammad Siddiq", "Field Marshal Muhammad Ayoub", "General Muhammad Zia-ul-Haq", "General Tikka Khan"],
+    answer: 1
+  }, // [22, 23]
+  {
+    question: "Who was the first Muslim C-in-C of Air Force?",
+    options: ["Air Marshal Asgher Khan", "Air Marshal Zafar Chahdhry", "Air Marshal Anwar Khan", "None of them"],
+    answer: 0
+  }, // [23]
+  {
+    question: "Who was the first Muslim C-in-C of Pakistan Navy?",
+    options: ["Admiral Mohammad Siddieque", "Admiral Shahid Kareem", "Admiral Karamat Rahman", "Admiral Tariq"],
+    answer: 0
+  }, // [23]
+  {
+    question: "The Largest cantonment of Pakistan army is located in ?",
+    options: ["Jhelum", "Kharian", "Karachi", "Multan"],
+    answer: 1
+  }, // [23]
+  {
+    question: "HIT stands for ?",
+    options: ["Heavy Industries Taxila", "Heavy Industries Tools", "Heavy Industries Tax", "None of them"],
+    answer: 0
+  }, // [23, 24]
+  {
+    question: "The range if Hatf-3 is ________ ?",
+    options: ["400km", "290km", "600km", "750km"],
+    answer: 1
+  }, // [24] (Source repetition)
+  {
+    question: "The range if Hatf-4 is _______ ?",
+    options: ["400km", "390km", "600km", "750km"],
+    answer: 3
+  }, // [24] (Source repetition)
+  {
+    question: "The latest version of Hatf-5 is ________ ?",
+    options: ["Ghauri-1", "Hataf", "Abdali", "Shaheen-1"],
+    answer: 0
+  }, // [24] (Source repetition)
+  {
+    question: "The Range of Ghari-1 is ________ ?",
+    options: ["1200-1500km", "1200-1300km", "1500-2000km", "1500-1800km"],
+    answer: 0
+  }, // [24] (Source repetition)
+  {
+    question: "The highest official rank of Pakistan Army is _________ ?",
+    options: ["Lieutenant", "Major General", "General", "None of them"],
+    answer: 2
+  }, // [25] (Source repetition)
+  {
+    question: "Who was the first Muslim C-in-C of Pakistan Army?",
+    options: ["Admiral Muhammad Siddiq", "Field Marshal Muhammad Ayoub", "General Muhammad Zia-ul-Haq", "General Tikka Khan"],
+    answer: 1
+  }, // [25] (Source repetition)
+  {
+    question: "Who was the first Muslim C-in-C of Air Force?",
+    options: ["Air Marshal Asgher Khan", "Air Marshal Zafar Chahdhry", "Air Marshal Anwar Khan", "None of them"],
+    answer: 0
+  }, // [25, 26] (Source repetition)
+  {
+    question: "Who was the first Muslim C-in-C of Pakistan Navy?",
+    options: ["Admiral Mohammad Siddieque", "Admiral Shahid Kareem", "Admiral Karamat Rahman", "Admiral Tariq"],
+    answer: 0
+  }, // [26] (Source repetition)
+  {
+    question: "The Largest cantonment of Pakistan army is located in ?",
+    options: ["Jhelum", "Kharian", "Karachi", "Multan"],
+    answer: 1
+  }, // [26] (Source repetition)
+  {
+    question: "HIT stands for ?",
+    options: ["Heavy Industries Taxila", "Heavy Industries Tools", "Heavy Industries Tax", "None of them"],
+    answer: 0
+  }, // [26] (Source repetition)
+  {
+    question: "In which of the following city the Pakistan Navel Staff College is located?",
+    options: ["Karachi", "Lahore", "Multan", "Rawalpindi"],
+    answer: 0
+  }, // [27]
+  {
+    question: "In which of the following city the Pakistan Navel War College is located?",
+    options: ["Karachi", "Lahore", "Islamabad", "Multan"],
+    answer: 1
+  }, // [27]
   ],
   geography: [
     {
